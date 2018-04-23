ce8f6a1ed5926cd1e7d56c91a68453d45d6a8527
437873573 <437873573@qq.com>
2018-04-23 19:22:44 +0800

    '首页滚动功能'

--- src/js/index.js
+++ src/js/index.js
@@ -10,5 +10,38 @@ $(function () {
     $('.dialog-wrapper .btn-close').click(()=>{
         $('.dialog-wrapper').hide()
     });
-    toTop()
+    toTop();
+    let i=0;
+    $('.rank-list .tab a').hover(function () {
+        $(this).addClass('current').siblings().removeClass('current');
+        i=$(this).index();
+        $('.cont').eq(i).show().siblings().hide();
+        $('.cont ul').css('top',0)
+    });
+    $('.rank-list .more').click(()=>{
+        let tlist=$('.rank-list .cont').eq(i).find('ul');
+        let l = tlist.css('top');
+        let tw = -parseInt(l) + 300;
+        if (parseInt(tlist.css('height')) > 300 && tw < parseInt(tlist.css('height'))) {
+            tlist.animate({top: `${parseInt(l) - 300}px`})
+        }
+    });
+    var tlist=$('.col-msjs .bd ul');
+    if (parseInt(tlist.css('width')) <= 1200) {
+        $('.col-msjs .prev').hide();
+        $('.col-msjs .next').hide();
+    }
+    $('.col-msjs .next').click(() => {
+        let l = tlist.css('left');
+        let tw = -parseInt(l) + 1200;
+        if (parseInt(tlist.css('width')) > 1200 && tw < parseInt(tlist.css('width'))) {
+            tlist.animate({left: `${parseInt(l) - 1200}px`})
+        }
+    });
+    $('.col-msjs .prev').click(() => {
+        let l = tlist.css('left');
+        if (parseInt(tlist.css('width')) > 1200 && parseInt(l) < 0) {
+            tlist.animate({left: `${parseInt(l) + 1200}px`})
+        }
+    });
 });
