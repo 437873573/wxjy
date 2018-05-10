b4d85ba93de55d8da4f5d034d789233d6b346be8
437873573 <437873573@qq.com>
2018-05-10 20:25:32 +0800

    '作文试题展示'

--- src/js/essayPractice.js
+++ src/js/essayPractice.js
@@ -6,13 +6,26 @@ $(function () {
         if (par.hasClass('show')) {
             par.removeClass('show').find('.essay-title').slideUp()
         } else {
-            par.addClass('show').find('.essay-title').slideDown()
+            let x=par.closest('ul').scrollTop()
+            par.addClass('show').find('.essay-title').slideDown(function () {
+
+            })
+
+            var li_show_count = par.prevAll('.show').size();
+            var li_hide_count = par.prevAll().size() - li_show_count;
+
+            var y = li_show_count * 349 + li_hide_count * 69;
+
+
+            // let y=x+280
+            par.closest('ul').animate({scrollTop:y})
         }
     })
     $('.question-list .but').click(function () {
         let par = $(this).closest('li')
         par.find('.essay-title').slideUp(function () {
-            par.off('click').find('.essay-content').slideDown().end().siblings().hide()
+            par.find('.essay-content').slideDown().end().siblings().hide().end().find('.icon-question-down').hide()
+            $('.question-list li h5').off('click')
         })
     })
     $('.answer-content div').click(function () {
@@ -20,30 +33,53 @@ $(function () {
         let n = $(this).index()
         $('.do-answer>div').eq(n).show().siblings().hide()
     })
-    var x='';
-    $('.cut').click(function () {
-        if(x==''){
-            pop('你没有选择任何单词','red')
+    $('.cut').click(()=>cut())
+    $('.paste').click(()=>paste())
+    $('.undo').click(()=>undo())
+    $('.redo').click(()=>redo())
+    function cut() {
+        try{
+            if(document.execCommand('cut',false,null)){
+                document.execCommand('cut')
+            }else{
+                pop('当前浏览器不支持复制，请手动复制','red')
+            }
+        }catch (err){
+            pop('当前浏览器不支持复制，请手动复制','red')
         }
-    });
-    $('.essay-content').mousedown(()=>{
-        if(x!=''){
-            x=''
+    }
+    function paste() {
+        $('.do-answer textarea').focus()
+        try{
+            if(document.execCommand('paste',false,null)){
+                document.execCommand('paste')
+            }else{
+                pop('当前浏览器不支持粘贴，请手动粘贴','red')
+            }
+        }catch (err){
+            pop('当前浏览器不支持粘贴，请手动粘贴','red')
         }
-    })
-    $('.essay-content').mouseup(()=>{
-        if(x==''){
-            x=funGetSelectTxt();
+    }
+    function undo() {
+        try{
+            if(document.execCommand('undo',false,null)){
+                document.execCommand('undo')
+            }else{
+                pop('当前浏览器不支持撤销，请手动撤销','red')
+            }
+        }catch (err){
+            pop('当前浏览器不支持撤销，请手动撤销','red')
         }
-    });
-    function funGetSelectTxt() {
-        var txt = "";
-        if (document.selection) {
-            txt = document.selection.createRange().text;    // IE
-        } else {
-            txt = document.getSelection();
+    }
+    function redo() {
+        try{
+            if(document.execCommand('redo',false,null)){
+                document.execCommand('redo')
+            }else{
+                pop('当前浏览器不支持恢复，请手动恢复','red')
+            }
+        }catch (err){
+            pop('当前浏览器不支持恢复，请手动恢复','red')
         }
-        let t = txt.toString().split(' ').shift()
-        return t;
-    };
+    }
 })
