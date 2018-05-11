e7d4e4d3f3925918774dafb1b353e6fe89121daa
437873573 <437873573@qq.com>
2018-05-11 20:23:53 +0800

    '作文字数统计'

--- src/js/essayPractice.js
+++ src/js/essayPractice.js
@@ -1,37 +1,9 @@
 import './modules/header';
 
 $(function () {
-    $('.question-list li h5').click(function () {
-        let par=$(this).closest('li')
-        if (par.hasClass('show')) {
-            par.removeClass('show').find('.essay-title').slideUp()
-        } else {
-            let x=par.closest('ul').scrollTop()
-            par.addClass('show').find('.essay-title').slideDown(function () {
-
-            })
-
-            var li_show_count = par.prevAll('.show').size();
-            var li_hide_count = par.prevAll().size() - li_show_count;
-
-            var y = li_show_count * 349 + li_hide_count * 69;
-
-
-            // let y=x+280
-            par.closest('ul').animate({scrollTop:y})
-        }
-    })
-    $('.question-list .but').click(function () {
-        let par = $(this).closest('li')
-        par.find('.essay-title').slideUp(function () {
-            par.find('.essay-content').slideDown().end().siblings().hide().end().find('.icon-question-down').hide()
-            $('.question-list li h5').off('click')
-        })
-    })
-    $('.answer-content div').click(function () {
-        $(this).addClass('active').siblings().removeClass('active')
-        let n = $(this).index()
-        $('.do-answer>div').eq(n).show().siblings().hide()
+    $('.text-do textarea').keyup(function () {
+        let x=$(this).val().length
+        $('.answer-options .count span').text(x)
     })
     $('.cut').click(()=>cut())
     $('.paste').click(()=>paste())
@@ -82,4 +54,90 @@ $(function () {
             pop('当前浏览器不支持恢复，请手动恢复','red')
         }
     }
+
+
+
+    //讨论与解析切换
+    $('.discuss .left header div').click(function () {
+        $(this).addClass('active').siblings().removeClass('active');
+        let index = $(this).index();
+        // console.log(index)
+        $('.discuss .left footer .content').eq(index).removeClass('hidden').siblings().addClass('hidden')
+    });
+    //评论
+    $('.post p').click(() => {
+        let comment = $('.post textarea').val();
+        $.ajax({
+            url: '/api/comment',
+            type: 'POST',
+            data: {
+                content: comment,
+                test_topic_id: collect[1],
+                test_volume_id: collect[0]
+            },
+            success: function (mess) {
+                if (mess.code == 0) {
+                    window.parent.pop('评论成功');
+                    window.location.reload()
+                }
+            }
+        })
+    });
+
+    //点赞
+    $('.feature span:first-of-type').click(function () {
+        // console.log($(this).find('b').text())
+        // console.log($(this).closest('li'))
+        let id = $(this).closest('li').data('commentId'), that = this;
+        if (!$(this).hasClass('added-zan')) {
+            $.post('/api/comment/vote', {comment_id: id, vote_type: 1}, function (mess) {
+                if (mess && mess.code === 0) {
+                    jia(that, 'icon-video-nozan', 'icon-video-zan')
+                }
+            })
+        } else {
+            $.post('/api/comment/vote', {comment_id: id, vote_type: 0}, function (mess) {
+                if (mess && mess.code === 0) {
+                    jian(that, 'icon-video-zan', 'icon-video-nozan')
+                }
+            })
+        }
+    });
+
+    function jia(that, c1, c2) {
+        $(that).addClass('added-zan').find('b').text(parseInt($(that).find('b').text()) + 1).end().find('i').removeClass(c1).addClass(c2)
+    }
+
+    function jian(that, c1, c2) {
+        $(that).removeClass('added-zan').find('b').text(parseInt($(that).find('b').text()) - 1).end().find('i').removeClass(c1).addClass(c2)
+    };
+    //参与评论文本区展开
+    $('.feature span:nth-of-type(2)').click(function () {
+        let h = $('body').outerHeight()
+        if (!$(this).hasClass('s')) {
+            $(`<div class="advice-adv clearfix">
+                    <textarea placeholder='请输入内容'></textarea>
+                    <p>提交</p>
+                </div>`).insertAfter($(this).closest('.advice-par'))
+            $(this).addClass('s').closest('.advice-par').siblings('.advice-adv').slideDown()
+            $('.iframe', window.parent.document).animate({height: h + 182}, 500);
+        } else {
+            $(this).removeClass('s').closest('.advice-par').siblings('.advice-adv').slideUp(() => {
+                $(this).closest('.advice-par').siblings('.advice-adv').remove();
+            });
+            $('.iframe', window.parent.document).animate({height: h - 142}, 500);
+        }
+    });
+    //参与评论提交
+    $('.advice').on('click', '.advice-adv p', function () {
+        let comment = $('.advice-adv textarea').val();
+        $.post('/api/comment', {
+            content: comment,
+            test_topic_id: collect[1],
+            test_volume_id: collect[0],
+            pid: id
+        }, function () {
+            window.location.reload()
+        })
+    });
 })
